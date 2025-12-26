ecded5523dcdb5c130505542756e60fef7030c84
Vinuthak <vinutha.kasaragod@gmail.com>
2025-12-26 01:13:52 +0100

    Add users list

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<template><div>RequestOPedia</div>
+  <hr />
+  <div v-for="user in userObj.users" :key="user.id">
+    <p>{{ user.name }}</p>
+    <p>{{ user.email }}</p>
+    <hr />
+  </div>
 </template>
+<script setup >
+  import axios from 'axios';
+  import { reactive, onMounted } from 'vue';
+  const userObj = reactive({
+    users: []
+  });
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  onMounted(() => {
+    axios.
+      get("https://jsonplaceholder.typicode.com/users").
+      then((response) => {
+        userObj.users = response.data;
+        console.log(response.data);
+      });
+  });
+</script>
